20f20a700e63adf48ae1302243447842f0b9df09
ladygidr <den.ladygin@gmail.com>
2024-10-28 10:21:28 +0100

    task2 commit

---
+++ python/2_basic_syntax/task_2.py
@@ -0,0 +1,8 @@
+# 1) Define two variables, "length" and "width," and give them the values representing the rectangle's length and width.
+# 2) Compute the area of the rectangle by multiplying the values stored in the "length" and "width" variables.
+# 3) Print the calculated area.
+
+length = 12.5
+width = 14.3
+
+print(f" Area of rectangle: {length * width}")
