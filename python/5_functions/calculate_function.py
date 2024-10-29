84dd375e0757d72cf9b1d2f316f3f25299dbb778
Dmytro-Chernukha <4epnyxa@gmail.com>
2024-10-29 11:06:20 +0100

    lesson5: homework_2 (Simple function that multiply two values and print the result to the console with additional text)

---
+++ python/5_functions/calculate_function.py
@@ -0,0 +1,10 @@
+def calculate_rectangle_area(length, width):
+    return length+width
+    #Simple function that multiply two values and print the result to the console with additional text
+
+
+print("The area of the rectangle - ", calculate_rectangle_area(2, 5))
+
+print("The area of the rectangle - ", calculate_rectangle_area(1.5,3.0))
+
+print("The area of the rectangle - ", calculate_rectangle_area(0, 0))
