8e9d2fdb66a83627555af07680dbc06a6106c55e
Hanna Semerenko <hsemeren@amazon.pl>
2024-10-29 09:52:59 +0100

    fruit count

--- python/0_exercises/fruits.py
+++ python/0_exercises/fruits.py
@@ -1,6 +1,34 @@
-fruits = ("Apple", "Apricot", "Apple", "Apricot", "Lemon", "Apple", "Apricot", "Lemon", "Avocado", "Banana")
-dict_counts = {}
-for fruit in fruits:
-    dict_counts.update({fruit: 1}) if fruit not in dict_counts.keys() \
-        else dict_counts.update({fruit: dict_counts[fruit] + 1})
-print(dict_counts)
+from typing import Tuple, Dict
+
+
+def count_fruits_1(fruits: Tuple[str, ...]) -> Dict[str, int]:
+    """
+    This function calculates the number of fruits in the first way.
+    The fruit key order by first inclusion in the tuple.
+    @param fruits: tuple of fruits
+    @return: the result of the calculation containing pairs of the name of the fruit and its quantity
+    """
+    dict_counts = {}
+    for fruit in fruits:
+        dict_counts.update({fruit: 1}) if fruit not in dict_counts.keys() \
+            else dict_counts.update({fruit: dict_counts[fruit] + 1})
+    return dict_counts
+
+
+def count_fruits_2(fruits: Tuple[str, ...]) -> Dict[str, int]:
+    """
+    This function calculates the number of fruits in the second way.
+    The fruit key order is random.
+    @param fruits: tuple of fruits
+    @return: the result of the calculation containing pairs of the name of the fruit and its quantity
+    """
+    dict_counts = {}
+    for fruit in set(fruits):
+        dict_counts[fruit] = fruits.count(fruit)
+    return dict_counts
+
+
+if __name__ == '__main__':
+    fruits_1 = ("Apple", "Apricot", "Apple", "Apricot", "Lemon", "Apple", "Apricot", "Lemon", "Avocado", "Banana")
+    print(f"Fruit counts (first way): {count_fruits_1(fruits_1)}")
+    print(f"Fruit counts (second way): {count_fruits_2(fruits_1)}")
