2fd14b56eb7ebff7839aa10c5b87f6bcbae3801e
innabiel <innabiel@amazon.pl>
2024-11-11 15:09:06 +0100

    Merge remote-tracking branch 'origin/inna-bielousova' into inna-bielousova

--- python/6_exception_files_debugging/task6.py
+++ python/6_exception_files_debugging/task6.py
@@ -1,18 +1,18 @@
-def doc_function(operation_type: str, file_path: str, content: str = None):
+def doc_function(operation_mode: str, file_path: str, content: str = None):
     """
     The function must perform actions with the file. In the case of reading, the content of the file should be displayed
     to the user.
 
-    @param operation_type: File operation type (writing to a file, reading from a file, etc.).
+    @param operation_mode: File operation type (writing to a file, reading from a file, etc.).
     @param file_path: File path (The path to the file with which the operation will be performed).
     @param content: Content (optional argument).
     """
-
+    operations = {"write": "w", "read": "r", "update": "a"}
     try:
-        with open(file_path, operation_type) as file:
-            if operation_type == 'r':
+        with open(file_path, operations[operation_mode]) as file:
+            if operations[operation_mode] == 'r':
                 print(file.read())
-            elif operation_type == 'w' or operation_type == 'a':
+            elif operations[operation_mode] == 'w' or operations[operation_mode] == 'a':
                 file.write(content)
             else:
                 print("No such operation type")
@@ -22,24 +22,27 @@ def doc_function(operation_type: str, file_path: str, content: str = None):
         print(f"[ValueError: My error description] {error}")
     except TypeError as error:
         print(f"[TypeError: My error description] {error}")
-    except Exception as error:
-        print(error)
+    except KeyError as error:
+        print(f"[KeyError: My error description] {error}")
+    # except Exception as error:
+    #     print(error)
 
 
 if __name__ == "__main__":
     print("Read 'test.txt' file:")
-    doc_function('r', './test.txt')
-    print("\nHandle 'FileNotFoundError':")
-    doc_function('r', './test1.txt')
+    doc_function('read', './test.txt')
     print("\n'New line' overwrite 'test.txt'")
-    doc_function('w', './test.txt', 'New line')
-    doc_function('r', './test.txt')
-    print("\nNew line added to the end of the 'test.txt'")
-    doc_function('a', './test.txt', '\nNew line 2')
-    doc_function('r', './test.txt')
+    doc_function('write', './test.txt', 'New line')
+    doc_function('read', './test.txt')
+    print("\nNew line 2 added to the end of the 'test.txt'")
+    doc_function('update', './test.txt', '\nNew line 2')
+    doc_function('read', './test.txt')
+
+    print("\nHandle 'FileNotFoundError':")
+    doc_function('read', './test1.txt')
+    print("\nHandle 'KeyError':")
+    doc_function('save', './test.txt')
     print("\nHandle 'ValueError':")
-    doc_function('s', './test.txt')
-    print("\nHandle 'ValueError':")
-    doc_function('r', './test.jpg')
+    doc_function('read', './test.jpg')
     print("\nHandle 'TypeError':")
-    doc_function('a', './test.txt', True)
+    doc_function('update', './test.txt', True)
