5702e9c426821378fbb8adef4cae8d7fbb6e0ff0
tezha <tezha@amazon.pl>
2024-11-13 11:36:38 +0100

    HW 3

---
+++ python/3_data_structures/homework_3.py
@@ -0,0 +1,11 @@
+fruits = ['apple', 'banana', 'cherry', 'potato'] #Create a list named fruits with the following items: 'apple', 'banana', 'cherry', 'potato'.
+colors = ('red', 'green', 'yellow') #Create a tuple named colors with three colors of your choice.
+numbers = {1, 2, 3} #Create a set named numbers with the numbers from 1 to 3.
+person = {'name': 'Tania', 'age': 30} #Create a dictionary named person with the following key-value pairs: 'name' and age.
+fruits.append('grape') #Add 'grape' to the fruits list.
+fruits.remove('potato')  #Remove 'potato' from the fruits list.
+is_apple_in_list = 'apple' in fruits # Check if 'apple' is in the list
+print(is_apple_in_list) # Output the result
+colors_length = len(colors) #Calculate the length of the colors tuple and output it.
+numbers.add(4) #Add the number 4 to the numbers set.
+person.update({'name': 'Marko', 'age': 34}) #Create a new key-value pair in the person dictionary (with new 'name' and age).
