274a24fe3b367045ea33e1a25399ee1b09a8548f
ksenkozl <ksenkozl@amazon.pl>
2024-11-18 14:10:04 +0100

    OOP p1 Homework.py

--- python/9_oop_pt2/OOP p2 Homework.py
+++ python/9_oop_pt2/OOP p2 Homework.py
@@ -1,13 +1,12 @@
 # Practice - coding:
-# Create a Python file with the next classes:
-# 1) Create the class "Zoo" with a protected list named "animals"
-# 2) Add methods for adding and removing elements from the list.
-# 3) Create classes for Wolf, Lion, Bison, Parrot, and Goose
-# Each class should have fields: name(str), id(int) and constructor.
-# Each class should have the method "play_sound()" which returns the string with the sound of the animal
-# (Use "Roar" for wolf and lion, "Moo" for bison, and "honk" for goose and parrot).
-# Create method which represents the current class object as a string e.g.
-# output in print command should be like "class Lion name Alex id 2".
+# Write a function that takes a list of numbers as input and returns a new list containing the square of each number using
+# the functional approach.
+# Write a function that takes a list of numbers as input and returns a new list containing only the even numbers.
+# Write a function that calculates the factorial of a number using the functional approach.
+# Capitalize the first letter of each word in a sentence "hello world, how are you?".
+# Given a list of students and their marks as tuples:
+# scores = [("Alice", 85), ("Bob", 92), ("Charlie", 78), ("David", 95)]
+# Write a function that calculates the average score of the students in the list. The function should take the list of tuples as input and return the average score.
 from abc import ABC, abstractmethod
 
 
@@ -40,6 +39,7 @@ class Fauna(ABC):
     def play_sound(self):
         pass
 
+
 class Mammals(Fauna):
     def play_sound(self):
         return f'I am doing {self.play_sound}!'
@@ -54,17 +54,21 @@ class SoundRoar(Mammals):
     def my_sound(self):
         return f'I am doing {self.my_sound}!'
 
+
 class SoundHonk(Birds):
     def my_sound(self):
         return f'I am doing {self.my_sound}!'
+
 
 class SoundOther(Mammals, Birds):
     def my_sound(self):
         return f'I am doing {self.my_sound}!'
 
+
 class Wolf(SoundRoar):
     def play_sound(self):
         return "Roar!"
+
 
 class Lion(SoundRoar):
     def play_sound(self):
