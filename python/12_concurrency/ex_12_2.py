0fe1f8c5df46f137914962601738649a3487750f
Hanna Semerenko <hsemeren@amazon.com>
2024-11-28 12:26:20 +0100

    multithreading

---
+++ python/12_concurrency/ex_12_2.py
@@ -0,0 +1,41 @@
+# Multithreading, IO-bound work (30 points):
+# Create a function that sleeps for a random amount of time (<10 seconds), prints and returns the sleep duration.
+# Create another function that calls the previous one 20 times using multiple threads in parallel
+# (e.g. use "concurrent.futures.ThreadPoolExecutor" class).
+# Print the "total workload" time (the sum of outputs from all calls) and the "max workload" time (the longest task).
+# Print the elapsed time. It should be several times smaller than the "workload" time.
+import time
+from random import choice
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+
+def execution_time(func):
+    def wrapper():
+        start_time = time.time()
+        f = func()
+        print(f'Execution time is{time.time() - start_time: .2f} seconds')
+        return f
+    return wrapper
+
+
+def sleep_random_amount():
+    sleep_duration = choice(range(1, 10))
+    print(f"Sleep duration is {sleep_duration} seconds")
+    time.sleep(sleep_duration)
+    return sleep_duration
+
+
+@execution_time
+def sleep_parallel():
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        futures = [executor.submit(sleep_random_amount) for _ in range(20)]
+    results = [f.result() for f in as_completed(futures)]
+    total_workload = sum(results)
+    max_workload = max(results)
+    print(f"Total workload = {total_workload}")
+    print(f"Max workload = {max_workload}")
+
+
+if __name__ == '__main__':
+    execution_time(sleep_random_amount)()
+    sleep_parallel()
