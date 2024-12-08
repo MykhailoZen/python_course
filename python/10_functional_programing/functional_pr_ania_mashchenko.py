05ec382cc7156c190db9cf8ad326122a7de59a4f
ramashch <ramashch@amazon.pl>
2024-12-08 19:57:07 +0100

    homework 9 for review

--- python/10_functional_programing/functional_pr_ania_mashchenko.py
+++ python/10_functional_programing/functional_pr_ania_mashchenko.py
@@ -4,7 +4,32 @@
 # Write a function that calculates the factorial of a number using the functional approach.
 # Capitalize the first letter of each word in a sentence "hello world, how are you?".
 # Given a list of students and their marks as tuples:
-#
 # scores = [("Alice", 85), ("Bob", 92), ("Charlie", 78), ("David", 95)]
 # Write a function that calculates the average score of the students in the list. The function should take the
 # list of tuples as input and return the average score.
+
+from functools import reduce
+
+numbers_1 = [1,2,3,4,5,8,12,44]
+def square_numbers(numbers): return [x ** 2 for x in numbers]
+
+def even_numbers(numbers): return[x for x in numbers if x % 2 == 0]
+
+def factorial(n): return reduce(lambda x, y: x * y, range(1, n + 1))
+
+s = "hello world, how are you?".title()
+
+scores = [("Alice", 85), ("Bob", 92), ("Charlie", 78), ("David", 95)]
+def average_score(student_scores):
+    total_score = sum(score for _, score in student_scores)
+    total_students = len(student_scores)
+    return total_score / total_students
+
+
+if __name__ == "__main__":
+    print(square_numbers(numbers_1))
+    print(even_numbers(numbers_1))
+    print(factorial(5))
+    print(s)
+    print(average_score(scores))
+
