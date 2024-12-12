c6a665dc78bd4f774ffa276f5194e55c77c089f3
tetiana-zhaivoron <tetiana.zhaivoron@gmail.com>
2024-12-12 17:21:53 +0100

    HW 5 Functions

---
+++ python/5_functions/homework_5.py
@@ -0,0 +1,12 @@
+def hello():
+    print("Hello World!")
+def calculate_rectangle_area(length, width):
+    """
+    Calculates area of rectangle
+    :param length:
+    :param width:
+    :return: calculated area
+    """
+    return length*width
+hello()
+print(calculate_rectangle_area(8, 6))
